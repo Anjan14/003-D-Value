a281cf1baedea9d4f05a69e670621b1fa6105045
Anjan Roy <ar567s@missouristate.edu>
2024-06-11 17:34:37 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,7 +14,8 @@
 
 int main() {
     // Map to store d_spacing values and their corresponding outputs
-    std::unordered_map<double, std::string> d_spacing_map = {
+    std::unordered_map<double, std::string> d_spacing_map = 
+    {
         {4.68200, "Li: 1, Co: 0.91, O: 1.84"},
         {4.81233, "Li: 0.37, Co: 1, O: 2"},
         {4.74917, "Li: 0.58, Co: 1, O: 2"},
@@ -30,9 +31,12 @@ int main() {
 
     // Find the corresponding output in the map
     auto it = d_spacing_map.find(d_spacing);
-    if (it != d_spacing_map.end()) {
+    if (it != d_spacing_map.end()) 
+    {
         std::cout << it->second << std::endl;
-    } else {
+    } 
+    else 
+    {
         std::cout << "Wrong D-Spacing input value for (l, m, n)=(0, 0, 3)" << std::endl;
     }
 
